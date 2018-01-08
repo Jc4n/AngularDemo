c40a4c44e7a57d85e20659b540e39f2745da531c
Jc4n <neinjessifer@gmail.com>
2018-01-08 11:03:29 +0800

    學習@Input, @output, ngFor和ngIf

--- angularDemo/src/app/add-form/add-form.component.ts
+++ angularDemo/src/app/add-form/add-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-form',
@@ -6,13 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-form.component.css']
 })
 export class AddFormComponent implements OnInit {
+  @Output() addTodoItem = new EventEmitter();
   placeholderText = '請輸入代辦喔!';
   todoText = '';
   constructor() { }
 
   ngOnInit() {
   }
-  addTodo() {
-    console.log('輸入的文字是~~~:' + this.todoText);
+  addTodo($event: MouseEvent) {
+    this.addTodoItem.emit(this.todoText);
   }
 }
--- angularDemo/src/app/todo-items/todo-items.component.ts
+++ angularDemo/src/app/todo-items/todo-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './../shared/todo-item';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-items',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-items.component.css']
 })
 export class TodoItemsComponent implements OnInit {
+  @Input() items: TodoItem[];
+  @Output() delTodoItem = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  chkItem(item: TodoItem) {
+    item.done = !item.done;
+  }
+  delItem(id:number){
+    this.delTodoItem.emit(id);
+  }
 }
