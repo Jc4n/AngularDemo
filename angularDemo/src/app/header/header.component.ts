515f3a2ad7c42dfbfa02380ace6c45d2e2aed259
Jc4n <neinjessifer@gmail.com>
2018-01-08 11:16:24 +0800

    學習NgClass

--- angularDemo/src/app/header/header.component.ts
+++ angularDemo/src/app/header/header.component.ts
@@ -6,12 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title ='welcome!!!';
+  title = 'welcome!!!';
   constructor() { }
 
   ngOnInit() {
   }
   getDate() {
     return (new Date());
+  }
+  getBlueClass() {
+    // 這裡可以用程式動態決定要回傳字串、陣列或物件
+    return 'blue';
   }
 }
